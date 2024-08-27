664e77a67c411d151c16f170d209e8cafe256f64
SETSOFIA NUSETOR <113457658+Funky2003@users.noreply.github.com>
2024-08-27 11:23:21 +0000

    Delete src directory

--- src/main/java/com/example/blogging/blog/entities/Tags.java
+++
@@ -1,9 +0,0 @@
-package com.example.blogging.blog.entities;
-
-import jakarta.persistence.Embeddable;
-
-import java.util.List;
-@Embeddable
-public record Tags(
-        List<String> tags
-) {}
--- src/main/java/com/example/blogging/blog/repositories/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.blogging.blog.repositories;
-
-import com.example.blogging.blog.entities.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<Blog, String> { }
